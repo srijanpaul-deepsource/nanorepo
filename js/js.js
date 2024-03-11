553d0a1edda5ed18833c12fa18940df453320cc9
Srijan Paul <srijan.paul@deepsource.io>
2024-03-11 15:09:00 +0530

    add ds toml

--- js/js.js
+++ js/js.js
@@ -1,8 +1,15 @@
-const firstName = "Srijan"
-const lastName = "Paul"
 
+/**
+ * @param {string} first first name
+ * @param {string} last  last name
+ * @returns full name
+ */
 function fullName(first, last) {
 	return `${first} ${last}`
 }
 
+const firstName = "Srijan"
+const lastName = "Paul"
 export const myName = fullName(firstName, lastName)
+
+debugger;

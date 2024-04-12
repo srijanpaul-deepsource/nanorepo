b64a93bf56b08fd809d6278be9eb5dd2eee53352
Srijan Paul <srijan.paul@deepsource.io>
2024-04-12 10:56:01 +0530

    testing

---
+++ hello.js
@@ -0,0 +1,13 @@
+export default function foo() {
+  if (true) return 123
+  return 456
+}
+
+export function abr() {
+  return 123 
+}
+
+function foobar() {
+  return 13
+}
+
--- js/Accounts.js
+++
@@ -1,3 +0,0 @@
-export function noDocs() {
-  return 1234;
-}
